2ffe9a3b511ff0281a145846f126c52a2465a343
João Lucas <jlucasoc@hotmail.com>
2022-06-04 01:11:24 -0300

    Primeiro design 👨‍🎨

---
+++ helper.py
@@ -0,0 +1,38 @@
+from math import sqrt
+
+
+def verifica_primo(num):
+  """
+    Verifica se o numero digitado é primo, retornando True se for, False se não for.
+  """
+
+  # Retorna True se em qualquer uma das iterações, nesse caso, da variável "i", o "num" atende a condição, e False se não. No final inverte o booleano.
+
+  return not any((num % i == 0 and i != num and i != 1) or num == 1 for i in range(1, num+1))
+
+
+def verifica_coprimo(a, b):
+    """
+    Essa função verifica se os numeros digitados pelo usuário são coprimos (Por meio do algoritmo de euclides).
+    Dois numeros podem ser ditos coprimos, quando o mdc entre os dois for 1.
+    """
+
+    if a % b == 0: # Se o resto da divisão é 0 retorna
+      return True if b == 1 else False # Se o b for 1, é coprimo
+
+    return verifica_coprimo(b, a % b)
+
+
+def retorna_coprimos(n, i):
+    """
+    Essa função calcula os coprimos de um número e o retorna ao usuário.
+    """
+
+    if i == n:
+        return
+
+    if verifica_coprimo(n, i) == 1:
+        print(f'{i}, ')
+    
+    return retorna_coprimos(n, i + 1)
+
--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from design import *
+from helper import *
 
 
-class App(QMainWindow):
+class Criptografia(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent) 
-        self.setWindowTitle('Programinha')
-        self.cw = QWidget()
-        self.vezes = 0
-        self.grid = QGridLayout(self.cw)
-        self.setStyleSheet("background-color: #ff0000;color:white;")
-        self.setFixedSize(370, 200)
-
-        def adddale():
-            self.vezes+=1
-            self.texto.setText(str(self.vezes))
-
-        self.btn = QPushButton('Fodase 🖕️')
-        self.btn.clicked.connect(adddale)
-        self.grid.addWidget(self.btn, 0, 0, 4, 4)
-        
-        self.texto = QLabel(str(self.vezes))
-        self.grid.addWidget(self.texto, 5, 3, 1,1 )
-
-
-        self.setCentralWidget(self.cw)
-
+        super().__init__(parent)
+        super().setupUi(self)
 
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
-    app = App()
-    app.show()
+    rsa = Criptografia()
+    rsa.show()
     qt.exec_()
